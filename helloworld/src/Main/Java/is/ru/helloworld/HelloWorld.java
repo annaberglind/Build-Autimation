df63af8e25bd5bf3580ab1b4d0b740ce1e677f2f
annaberglind <annaj11@ru.is>
2013-11-17 17:04:29 +0000

    adda spark depency í pom skrá

--- helloworld/src/Main/Java/is/ru/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package is.ru.helloworld;
- 
- 
- class HelloWorld {
-     public static void main(String[] args) {
-         System.out.println("Hello world!");
-     }
- }
- 
--- helloworld/src/Main/Java/is/ru/helloworld/World.java
+++
@@ -1,9 +0,0 @@
-package is.ru.helloworld;
- 
- 
- class World {
-     public String greet() {
-         return "Hello world";
-     }
- }
- 
